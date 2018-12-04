749c66f1b557332fa87ea1fbad4637ee4ef6d135
Erik Weiss <mail@technicallyerik.com>
2018-12-03 22:58:18 -0600

    Day 3

---
+++ 2018/Day3/part2.rs
@@ -0,0 +1,57 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+
+fn main() {
+	let file = File::open("input.txt").expect("No such file");;
+	let buf = BufReader::new(file);
+	let strings: Vec<String> = buf.lines()
+									.map(|l| l.expect("Could not parse line"))
+									.collect();
+									
+	let mut fabric = [[0u16; 1000]; 1000];
+									
+	for string in &strings {
+		let stringParts: Vec<&str> = string.split_whitespace().collect();
+		let number = stringParts[0];
+		let coordinates = str::replace(stringParts[2], ":", "");
+		let coordinateParts: Vec<&str> = coordinates.split(",").collect();
+		let inchesFromLeft = coordinateParts[0].parse::<u16>().unwrap();
+		let inchesFromTop = coordinateParts[1].parse::<u16>().unwrap();
+		let size = stringParts[3];
+		let sizeParts: Vec<&str> = size.split("x").collect();
+		let width = sizeParts[0].parse::<u16>().unwrap();
+		let height = sizeParts[1].parse::<u16>().unwrap();
+		
+		for li in inchesFromLeft..(inchesFromLeft + width) {
+			for ti in inchesFromTop..(inchesFromTop + height) {
+				fabric[li as usize][ti as usize] += 1;
+			}
+		}
+	}
+	
+	for string in strings {
+		let stringParts: Vec<&str> = string.split_whitespace().collect();
+		let number = stringParts[0];
+		let coordinates = str::replace(stringParts[2], ":", "");
+		let coordinateParts: Vec<&str> = coordinates.split(",").collect();
+		let inchesFromLeft = coordinateParts[0].parse::<u16>().unwrap();
+		let inchesFromTop = coordinateParts[1].parse::<u16>().unwrap();
+		let size = stringParts[3];
+		let sizeParts: Vec<&str> = size.split("x").collect();
+		let width = sizeParts[0].parse::<u16>().unwrap();
+		let height = sizeParts[1].parse::<u16>().unwrap();
+		
+		let mut overlap = false;
+		for li in inchesFromLeft..(inchesFromLeft + width) {
+			for ti in inchesFromTop..(inchesFromTop + height) {
+				if fabric[li as usize][ti as usize] > 1 {
+					overlap = true;
+				}
+			}
+		}
+		if !overlap {
+			print!("{}", number);
+		}
+	}
+}
--- 2018/README.md
+++ 2018/README.md
@@ -1,2 +1,7 @@
+## Context
+For the 2018 Advent of Code, I thought it would be fun to try a different programming language for each day, some of which I had little or no experience in.  This code should not be evaluated in terms of cleverness, efficency, or syntax.
+
+## Days
 * Day 1 - Swift
 * Day 2 - Ruby
+* Day 3 - Rust
